deaf1b03586a66903ae4952fe559e46a86de5011
Maxime RIO <maxime.rio@unibas.ch>
2016-03-24 14:02:09 +0100

    more tests

--- TestTensorStack.m
+++ TestTensorStack.m
@@ -1,12 +1,14 @@
+% Test suite for TensorStack
+% Use 'run(TestTensorStack)' to run the whole suite.
 classdef TestTensorStack < matlab.unittest.TestCase
    
     properties
         stack       % tested TensorStack
-        real_stack  % Corresponding regular array
+        real_stack  % corresponding regular array
     end
     
     properties (TestParameter)
-        % test indexing
+        % tested indexing schemas
         subs = struct( ...
             'all', {{':', ':', ':'}}, ...
             'flat', {{':'}}, ...
@@ -21,14 +23,14 @@ classdef TestTensorStack < matlab.unittest.TestCase
             'part5', {{1:3, 3, 4:5}}, ...
             'part6', {{':', 3:end, 4:5}}, ...
             'part7', {{':', [3, 1, 2], ':'}});
-        % test permutations
+        % tested permutations
         order = struct( ...
             'unchanged', [1, 2, 3], ...
             'reverse', [3, 2, 1], ...
             'invert1', [2, 1, 3], ...
             'invert2', [1, 3, 2], ...
             'shuffle', [2, 3, 1]);
-        % test reshaping (split only)
+        % tested new size for reshaping (split only)
         newsize = struct( ...
             'unchanged', [3, 12, 5], ...
             'unknown1', {{3, [], 5}}, ...
@@ -44,10 +46,14 @@ classdef TestTensorStack < matlab.unittest.TestCase
             'split5', [3, 3, 2, 2, 5]);
         % seed for random permutations
         permseed = {1, 22, 542, 445, 55324};
+        % tested splits for 12
+        splits = { ...
+            [2, 6], [6, 2], [3, 4], [4, 3], [2, 2, 3], [3, 2, 2], [2, 3, 2]};
     end
 
     methods (TestMethodSetup)
         function createStack(testCase)
+            % tested data, as a concatenation of 3 arrays
             rng(1)
             data1 = randn(3, 4, 5);
             data2 = randn(3, 7, 5);
@@ -58,47 +64,83 @@ classdef TestTensorStack < matlab.unittest.TestCase
     end
 
     methods (Test)
+        % test indexing
         function testSubsref(testCase, subs)
             testCase.verifyEqual( ...
                 testCase.stack(subs{:}), testCase.real_stack(subs{:}));
         end
 
+        % test permutation
         function testPermute(testCase, order)
+            % permute stack
             pstack = permute(testCase.stack, order);
             preal = permute(testCase.real_stack, order);
             testCase.verifySize(pstack, size(preal));
-            testCase.verifyEqual(pstack(:, :, :), preal(:, :, :));
+            testCase.verifyEqual(pstack(:, :, :), preal);
+
+            % restore original order
+            ppstack = ipermute(pstack, order);
+            testCase.verifySize(ppstack, size(testCase.real_stack));
+            testCase.verifyEqual(ppstack(:, :, :), testCase.real_stack);
         end
         
+        % test reshaping
         function testReshape(testCase, newsize)
             if iscell(newsize)
-                pstack = reshape(testCase.stack, newsize{:});
-                preal = reshape(testCase.real_stack, newsize{:});
+                rstack = reshape(testCase.stack, newsize{:});
+                rreal = reshape(testCase.real_stack, newsize{:});
             else
-                pstack = reshape(testCase.stack, newsize);
-                preal = reshape(testCase.real_stack, newsize);
+                rstack = reshape(testCase.stack, newsize);
+                rreal = reshape(testCase.real_stack, newsize);
             end
-            testCase.verifySize(pstack, size(preal));
-            testCase.verifyEqual(pstack(:, :, :), preal(:, :, :));
+            colons = repmat({':'}, ndims(rreal), 1);
+            testCase.verifySize(rstack, size(rreal));
+            testCase.verifyEqual(rstack(colons{:}), rreal);
         end
 
+        % test reshaping + random permutation
         function testReshapePermute(testCase, newsize, permseed)
             if iscell(newsize)
-                pstack = reshape(testCase.stack, newsize{:});
-                preal = reshape(testCase.real_stack, newsize{:});
+                rstack = reshape(testCase.stack, newsize{:});
+                rreal = reshape(testCase.real_stack, newsize{:});
             else
-                pstack = reshape(testCase.stack, newsize);
-                preal = reshape(testCase.real_stack, newsize);
+                rstack = reshape(testCase.stack, newsize);
+                rreal = reshape(testCase.real_stack, newsize);
             end
 
+            % random permutation
             rng(permseed)
-            rorder = randperm(ndims(preal));
-            pstack = permute(pstack, rorder);
-            preal = permute(preal, rorder);
+            rorder = randperm(ndims(rreal));
+            pstack = permute(rstack, rorder);
+            preal = permute(rreal, rorder);
 
+            colons = repmat({':'}, ndims(preal), 1);
             testCase.verifySize(pstack, size(preal));
-            testCase.verifyEqual(pstack(:, :, :), preal(:, :, :));
-        end
+            testCase.verifyEqual(pstack(colons{:}), preal);
 
+            % restore shape before permutation
+            ppstack = ipermute(pstack, rorder);
+            colons = repmat({':'}, ndims(rreal), 1);
+            testCase.verifySize(ppstack, size(rreal));
+            testCase.verifyEqual(ppstack(colons{:}), rreal);
+        end
+        % test permutation + reshape
+        function testPermuteReshape(testCase, order, splits)
+            % permutation
+            pstack = permute(testCase.stack, order);
+            preal = permute(testCase.real_stack, order);
+
+            % split dimension who is equal to 12
+            psize = size(preal);
+            split_dim = find(psize == 12);
+            rsize = [psize(1:(split_dim-1)), splits, psize((split_dim+1:end))];
+
+            % reshape and test
+            rstack = reshape(pstack, rsize);
+            rreal = reshape(preal, rsize);
+            colons = repmat({':'}, ndims(rreal), 1);
+            testCase.verifySize(rstack, size(rreal));
+            testCase.verifyEqual(rstack(colons{:}), rreal);
+        end
     end
 end
